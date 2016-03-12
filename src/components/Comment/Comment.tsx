52f8d13774eb66f17bee5ef67f23e8a4753862df
donttellmywife <volodymyr.puchkov@gmail.com>
2016-03-12 15:03:09 +0200

    Question component flex styles

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,32 +1,29 @@
-'use strict';
-
 import * as React from "react";
 
 const styles: any = require('./comment.css');
 const color: any = require('../../theme/color.css');
 
-export class Comment extends React.Component<{
-    author: 'KobeJohn',
-    content: 'If you come back to this, maybe you should change the accepted answer from diaspora.',
-    date: 'Feb 5 12',
-    time: '13:35'
-}, void> {
+interface CommentProps {
+    author: string;
+    content: string;
+    date: string;
+}
+
+export class Comment extends React.Component<CommentProps, void> {
 
     constructor(props) {
         super(props);
     }
 
     public render() {
-        const author = 'KobeJohn',
-            content = 'If you come back to this, maybe you should change the accepted answer from diaspora.',
-            date = 'Feb 5 12',
-            time = '13:35';
+        const author = this.props.author || 'KobeJohn',
+            content = this.props.content || 'If you come back to this, maybe you should change the accepted answer from diaspora.',
+            date = this.props.date || 'Feb 5 12 at 13:35';
         return (
             <div className={styles.comment}>
                 <span>{content} - </span>
-                <a href="link_to_user" className={color.link}>{author}</a>
+                <a href="#" className={color.link}>{author}</a>
                 <span className={color.helper}> {date}</span>
-                <span className={color.helper}> at {time}</span>
             </div>
         );
     }
--- src/components/Question/Question.tsx
+++ src/components/Question/Question.tsx
@@ -1,22 +1,83 @@
 import * as React from "react";
 
-export class Question extends React.Component<{}, void> {
+import Comment from '../Comment/Comment.tsx';
+
+const css: any = require('./question.scss');
+const color: any = require('../../theme/color.css');
+// const commonCSS: any = require('../../theme/common.scss');
+
+interface CommentProps {
+    author: string;
+    content: string;
+    date: string;
+}
+
+interface QuestionProps extends React.Props<Question> {
+    title?: string;
+    content?: string;
+    author?: string;
+    date?: string;
+    tags?: string[];
+    comments?: CommentProps[];
+    onAddComment?: (val: string) => any;
+}
+
+export class Question extends React.Component<QuestionProps, void> {
 
     constructor(props) {
         super(props);
     }
 
     public render() {
-        const author = 'KobeJohn',
-            content = 'If you come back to this, maybe you should change the accepted answer from diaspora.',
-            date = 'Feb 5 12',
-            time = '13:35';
+        const author = this.props.author || 'Madara Uchiha',
+            title = this.props.title || 'Why shouldnt I use mysql_* functions in PHP?',
+            content = this.props.content || 'Why should I use something else even if they work on my site?',
+            date = this.props.date || 'Feb 5 12';
+        const fakeComment1 = {
+            author: 'Sergiy Kojedubov',
+            content: 'Да, все так и есть.',
+            date: 'Jan 13 16 at 13:14'
+        };
+
+        const fakeComment2 = {
+            author: 'Андрей Поддубный',
+            content: 'Nothing like that, I know better solutions',
+            date: 'May 13 16 at 09:11'
+        };
+
+        const hasTags = false; // const hasTags = this.props.tags.length > 0;
+        const tags = hasTags
+                ? this.props.tags.map(function(tag) {
+                    return <a href="#" className={css.tag}>tag</a>
+                })
+                : ['slq', 'php', 'databases'].map(function(tag) {
+                    return <a href="#" className={css.tag}>{tag}</a>
+                });
+
+        const hasComments = false; // const hasComments = this.props.comments.length > 0;
+        const comments = hasComments
+                ? this.props.comments.map(function(comment) {
+                    return <Comment
+                                author={comment.author}
+                                content={comment.content}
+                                date={comment.date} />
+                })
+                : [fakeComment1, fakeComment2].map(function(comment) {
+                    return <Comment
+                                author={comment.author}
+                                content={comment.content}
+                                date={comment.date} />
+                });
+
         return (
-            <div className="comment">
-                <span className="text">{content} - </span>
-                <a href="link_to_user" className="author">{author}</a>
-                <span className="date"> {date}</span>
-                <span className="time"> at {time}</span>
+            <div className={css.root}>
+                <header>
+                    <a href="#" className={css.title}>{title}</a>
+                    <section>{tags}</section>
+                </header>
+                <div className={css.content}>{content}</div>
+                <section className={css.owner}>{author}</section>
+                <section className={css.comments}>{comments}</section>
             </div>
         );
     }
