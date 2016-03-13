237d52abb7afb99536489a1280609dc111ed768a
Shadowgun <shadowgun@i.ua>
2016-03-13 23:15:52 +0200

    question api full test coverage

--- src/main/server/api/Question.ts
+++ src/main/server/api/Question.ts
@@ -1,5 +1,6 @@
 import {Question} from '../../client/redux/entities';
 import {Router} from 'express';
+import {assign} from 'lodash';
 
 const dummyQuestion: Question = {
     id: 0,
@@ -13,10 +14,10 @@ const dummyQuestion: Question = {
 };
 export const router = Router();
 
-router.get("/questions", () => [dummyQuestion]);
-router.get("/questions/:id", (req, res, id) => (id) ? res.status(200).json(dummyQuestion).end() : res.status(404).end('NOT FOUND'));
-router.post("/questions", (req, res) => (req.body && req.body.id === undefined) ? res.status(200).json(dummyQuestion) : res.status(404).end('NOT FOUND'));
-router.put("/questions", (req, res) => (req.body && req.body.id !== undefined) ? res.status(200).json(dummyQuestion) : res.status(404).end('NOT FOUND'));
-router.delete("/questions/:id", (req, res, id) => (!req.body && id) ? res.status(200).json(dummyQuestion) : res.status(404).end('NOT FOUND'));
+router.get("/questions", (req, res) => (req.body) ? res.status(200).json([dummyQuestion]) : res.status(404).end('NOT FOUND'));
+router.get("/questions/:id", (req, res) => (req.body && dummyQuestion.id === Number(req.params.id)) ? res.status(200).json(dummyQuestion).end() : res.status(404).end('NOT FOUND'));
+router.post("/questions", (req, res) => (req.body && req.body.id === undefined) ? res.status(200).json(assign({}, dummyQuestion, req.body, { id: 1})).end() : res.status(404).end('NOT FOUND'));
+router.put("/questions", (req, res) => (req.body && dummyQuestion.id === Number(req.body.id)) ? res.status(200).json(assign({}, dummyQuestion, req.body)).end() : res.status(404).end('NOT FOUND'));
+router.delete("/questions/:id", (req, res) => (req.body && dummyQuestion.id === Number(req.params.id)) ? res.status(204).end() : res.status(404).end('NOT FOUND'));
 
 export default router;
--- src/test/server/api/question.test.ts
+++ src/test/server/api/question.test.ts
@@ -1,9 +1,8 @@
 /// <reference path='../../../../typings/main.d.ts'/>
 
-import {expect, should} from 'chai';
+import {expect} from 'chai';
 import * as request from 'supertest';
 import * as Server from '../../../main/server';
-import {Question} from '../../../main/client/redux/entities';
 
 
 describe("Routes", function() {
@@ -11,13 +10,30 @@ describe("Routes", function() {
 
     before(done => {
         Server.run();
-        should();
         done();
     });
 
     describe("Questions API", function() {
 
-        it("should respond", done => {
+        it("should respond with list of entities", done => {
+            request(url)
+                .get('/questions')
+                .expect(200) // Status code
+                .expect('Content-Type', /json/)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(res.body).be.a("Array");
+                    expect(res.body[0]).have.property('id');
+                    expect(res.body[0].id).to.be.equals(0);
+
+                    done();
+                });
+        });
+
+        it("should respond with entity", done => {
             request(url)
                 .get('/questions/0')
                 .expect(200) // Status code
@@ -26,10 +42,83 @@ describe("Routes", function() {
                     if (err) {
                         throw err;
                     }
-                    // Should.js fluent syntax applied
+
                     expect(res.body).to.be.not.undefined;
-                    res.body.should.have.property('id');
+                    expect(res.body).have.property('id');
                     expect(res.body.id).to.be.equals(0);
+
+                    done();
+                });
+        });
+
+        it("should create entity", done => {
+            request(url)
+                .post('/questions')
+                .send({
+                    title: "New 1",
+                    content: "Hello world",
+                    createdBy: "string",
+                    createdDate: new Date(Date.parse("2016-03-12T20:25:47.800Z")),
+                    lastModifiedBy: "string",
+                    lastModifiedDate: new Date(Date.parse("2016-03-12T20:25:47.800Z")),
+                    rating: 0
+                })
+                .expect(200) // Status code
+                .expect('Content-Type', /json/)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(res.body).to.be.not.undefined;
+                    expect(res.body).have.property('id');
+                    expect(res.body.id).to.be.equals(1);
+                    expect(res.body.title).to.be.equals("New 1");
+
+                    done();
+                });
+        });
+
+        it("should update entity", done => {
+            request(url)
+                .put('/questions')
+                .send({
+                    id: 0,
+                    title: "New 2",
+                    content: "Hello world",
+                    createdBy: "string",
+                    createdDate: new Date(Date.parse("2016-03-12T20:25:47.800Z")),
+                    lastModifiedBy: "string",
+                    lastModifiedDate: new Date(Date.parse("2016-03-12T20:25:47.800Z")),
+                    rating: 0
+                })
+                .expect(200) // Status code
+                .expect('Content-Type', /json/)
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(res.body).to.be.not.undefined;
+                    expect(res.body).have.property('id');
+                    expect(res.body.id).to.be.equals(0);
+                    expect(res.body.title).to.be.equals("New 2");
+
+                    done();
+                });
+        });
+
+        it("should delete entity", done => {
+            request(url)
+                .delete('/questions/0')
+                .expect(204) // Status code
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    expect(res.body).to.be.empty;
+
                     done();
                 });
         });
