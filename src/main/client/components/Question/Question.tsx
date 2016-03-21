bae6334d47d0cb67ab9c645a322212cc3b518ef7
Vladimir Puchkov <Volodymyr.Puchkov@playtech.com>
2016-03-21 17:14:18 +0200

    continue

--- src/main/client/components/Question/Question.tsx
+++ src/main/client/components/Question/Question.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 import * as Entities from '../../entities';
-import { Comment } from '../../components';
+import { Comment, Editor } from '../../components';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Actions as CommentActions } from '../../redux/modules/Comment/index';
+
+
 
 const css = require('./Question.scss');
 
@@ -8,9 +14,29 @@ interface QuestionProps extends React.Props<Question>, Entities.Question {
     comments?: Entities.Comment[];
 }
 
-export class Question extends React.Component<QuestionProps, void> {
-    public test() {
-        console.log('ttt');
+@connect(
+    state => ({
+        comments: state.comments,
+        text: state.editor.content
+     }),
+    dispatch => bindActionCreators({ CommentActions }, dispatch)
+)
+
+export class Question extends React.Component<QuestionProps, any> {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            commentsID: 0
+        }
+    }
+
+    addComment() {
+        let comment: Entities.Comment = {
+            content: this.state.text,
+            id: 1
+        }
+        this.setState({'commentsID': ++this.state.commentsID})
+        CommentActions.create(0, comment);
     }
 
     public render() {
@@ -25,7 +51,7 @@ export class Question extends React.Component<QuestionProps, void> {
                 <div className={css.content}>{content}</div>
                 <section className={css.owner}>{createdBy}</section>
                 <section className={css.comments}>{comments && comments.map(comment => <Comment {...comment} />) }</section>
-                <button>Add</button>
+
             </div>
         );
     }
